b837fcc461084e2740dcd03c25df1ed1eb14b679
DPoSign <daniel.poinsignon@gmail.com>
2016-11-02 10:24:07 +0100

    Test

--- app/config/parameters_production.php
+++ app/config/parameters_production.php
@@ -1,9 +1,15 @@
 <?php
-$db = parse_url(getenv('CLEARDB_DATABASE_URL'));
+    $db = parse_url(getenv('CLEARDB_DATABASE_URL'));
 
-$container->setParameter('database.driver', getenv('SYMFONY_BD_DRIVER'));
-$container->setParameter('database.host', getenv('SYMFONY_BD_HOST'));
-$container->setParameter('database.port', getenv('SYMFONY_BD_PORT'));
-$container->setParameter('database.name', getenv('SYMFONY_BD_NAME'));
-$container->setParameter('database.user', getenv('SYMFONY_BD_USER'));
-$container->setParameter('database.password', getenv('SYMFONY_BD_PASS'));
+    $container->setParameter('database_driver', 'pdo_mysql');
+    $container->setParameter('database_host', $db['host']);
+    $container->setParameter('database_port', $db['port']);
+    $container->setParameter('database_name', substr($db["path"], 1));
+    $container->setParameter('database_user', $db['user']);
+    $container->setParameter('database_password', $db['pass']);
+    $container->setParameter('secret', getenv('SECRET'));
+    $container->setParameter('locale', 'en');
+    $container->setParameter('mailer_transport', null);
+    $container->setParameter('mailer_host', null);
+    $container->setParameter('mailer_user', null);
+    $container->setParameter('mailer_password', null);
