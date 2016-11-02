4fcd885469e2fd1cf486549a6e457f1109eff627
DPoSign <daniel.poinsignon@gmail.com>
2016-11-03 00:36:41 +0100

    Adding host

--- app/config/parameters_production.php
+++ app/config/parameters_production.php
@@ -5,7 +5,7 @@
     
 
     $container->setParameter('database_driver', 'pdo_mysql');
-    $container->setParameter('database_host', $db['host']);
+    $container->setParameter('database_host', $db['5432']);
     $container->setParameter('database_port', $db['port']);
     $container->setParameter('database_name', substr($db["path"], 1));
     $container->setParameter('database_user', $db['user']);
