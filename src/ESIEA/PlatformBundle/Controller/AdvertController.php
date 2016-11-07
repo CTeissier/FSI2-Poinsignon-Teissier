423c24b494df3844cf0b683742189fd67fb80b5f
DPoSign <daniel.poinsignon@gmail.com>
2016-11-07 13:24:12 +0100

    Debug du formulaire de paiement

--- src/ESIEA/PlatformBundle/Controller/AdvertController.php
+++ src/ESIEA/PlatformBundle/Controller/AdvertController.php
@@ -9,168 +9,195 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class AdvertController extends Controller
-{
-  public function paypal_paymentAction()
+
   {
+
+  function paypal_paymentAction()
+    {
     $api_paypal = 'https://api-3t.sandbox.paypal.com/nvp?'; // Site de l'API PayPal. On ajoute déjà le ? afin de concaténer directement les paramètres.
     $version = 56.0; // Version de l'API
-    
     $user = 'esieaeats.com'; // Utilisateur API
     $pass = 'EEADHRPYSINXGDVM'; // Mot de passe API
     $signature = 'FKjyxmNtVWvHUmDEGgYszvtJlBrAYNbpjxTPeTTp-51ZsmLx6sS3'; // Signature de l'API
-
-    $api_paypal = $api_paypal.'VERSION='.$version.'&USER='.$user.'&PWD='.$pass.'&SIGNATURE='.$signature; // Ajoute tous les paramètres
-    return  $api_paypal; // Renvoie la chaîne contenant tous nos paramètres.
-  }
-
-  public function indexAction($page)
-  {
-    if ($page < 1) {
-     $page = 1;
+    $api_paypal = $api_paypal . 'VERSION=' . $version . '&USER=' . $user . '&PWD=' . $pass . '&SIGNATURE=' . $signature; // Ajoute tous les paramètres
+    return $api_paypal; // Renvoie la chaîne contenant tous nos paramètres.
     }
-    
+
+
+  function indexAction($page)
+    {
+    if ($page < 1)
+      {
+      $page = 1;
+      }
+
     $nbPerPage = 5;
-    $listAdverts = $this->getDoctrine()
-      ->getManager()
-      ->getRepository('ESIEAPlatformBundle:Advert')
-      ->getAdverts($page, $nbPerPage)
-    ;
-
+    $listAdverts = $this->getDoctrine()->getManager()->getRepository('ESIEAPlatformBundle:Advert')->getAdverts($page, $nbPerPage);
     $nbPages = ceil(count($listAdverts) / $nbPerPage);
+
     // Si la page n'existe pas, on retourne une 404
-    if ($page > $nbPages) {
-      throw $this->createNotFoundException("La recette ".$page." n'existe pas.");
-    }
+
+    if ($page > $nbPages)
+      {
+      throw $this->createNotFoundException("La recette " . $page . " n'existe pas.");
+      }
 
     return $this->render('ESIEAPlatformBundle:Advert:index.html.twig', array(
       'listAdverts' => $listAdverts,
-      'nbPages'     => $nbPages,
-      'page'        => $page,
+      'nbPages' => $nbPages,
+      'page' => $page,
     ));
-  }
-
-  public function viewAction($id)
-  {
-    $em = $this->getDoctrine()->getManager();
-
-    $advert = $em->getRepository('ESIEAPlatformBundle:Advert')->find($id);
-
-    if (null === $advert) {
-      throw new NotFoundHttpException("La recette d'id ".$id." n'existe pas.");
     }
-  
+
+
+  function viewAction($id)
+    {
+    $em = $this->getDoctrine()->getManager();
+    $advert = $em->getRepository('ESIEAPlatformBundle:Advert')->find($id);
+    if (null === $advert)
+      {
+      throw new NotFoundHttpException("La recette d'id " . $id . " n'existe pas.");
+      }
+
     return $this->render('ESIEAPlatformBundle:Advert:view.html.twig', array(
-      'advert'           => $advert,
-         ));
-  }
-  
-  public function loginAction($page)
-  {
+      'advert' => $advert,
+    ));
+    }
+
+
+  function loginAction($page)
+    {
     $pass_hache = sha1($_POST['pass']);
 
     // Vérification des identifiants
-    //TO-DO : BDD
+    // TO-DO : BDD
+
     $req->execute(array(
-        'pseudo' => $pseudo,
-        'pass' => $pass_hache));
-
+      'pseudo' => $pseudo,
+      'pass' => $pass_hache
+    ));
     $resultat = $req->fetch();
-
     if (!$resultat)
-    {
-        echo 'Mauvais identifiant ou mot de passe !';
+      {
+      echo 'Mauvais identifiant ou mot de passe !';
+      }
+      else
+      {
+      session_start();
+      $_SESSION['id'] = $resultat['id'];
+      $_SESSION['pseudo'] = $pseudo;
+      echo 'Vous êtes connecté !';
+      }
     }
-    else
+
+
+  function addAction(Request $request)
     {
-        session_start();
-        $_SESSION['id'] = $resultat['id'];
-        $_SESSION['pseudo'] = $pseudo;
-        echo 'Vous êtes connecté !';
-    }
-  }
-  public function addAction(Request $request)
-  {
     $advert = new Advert();
-    $form   = $this->get('form.factory')->create(AdvertType::class, $advert);
-    if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
+    $form = $this->get('form.factory')->create(AdvertType::class , $advert);
+
+    if ($request->isMethod('POST') && $form->handleRequest($request)->isValid())
+      {
       $em = $this->getDoctrine()->getManager();
       $em->persist($advert);
       $em->flush();
       $request->getSession()->getFlashBag()->add('notice', 'Recette bien enregistrée.');
-      return $this->redirectToRoute('esiea_platform_view', array('id' => $advert->getId()));
-    }
-    return $this->render('ESIEAPlatformBundle:Advert:add.html.twig', array(
-      'form' => $form->createView(),
-    ));
-  }
+      return $this->redirectToRoute('esiea_platform_view', array(
+        'id' => $advert->getId()
+      ));
+      }
 
-  public function editAction($id, Request $request)
-  {
+    return $this->render('ESIEAPlatformBundle:Advert:add.html.twig', array(
+      'form' => $form->createView() ,
+    ));
+    }
+
+
+  function editAction($id, Request $request)
+    {
     $em = $this->getDoctrine()->getManager();
     $advert = $em->getRepository('ESIEAPlatformBundle:Advert')->find($id);
-    if (null === $advert) {
-      throw new NotFoundHttpException("La recette d'id ".$id." n'existe pas.");
-    }
-    $form = $this->get('form.factory')->create(AdvertEditType::class, $advert);
-    if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
+    if (null === $advert)
+      {
+      throw new NotFoundHttpException("La recette d'id " . $id . " n'existe pas.");
+      }
+
+    $form = $this->get('form.factory')->create(AdvertEditType::class , $advert);
+
+    if ($request->isMethod('POST') && $form->handleRequest($request)->isValid())
+      {
       $em->flush();
       $request->getSession()->getFlashBag()->add('notice', 'Recette bien modifiée.');
-      return $this->redirectToRoute('esiea_platform_view', array('id' => $advert->getId()));
-    }
+      return $this->redirectToRoute('esiea_platform_view', array(
+        'id' => $advert->getId()
+      ));
+      }
+
     return $this->render('ESIEAPlatformBundle:Advert:edit.html.twig', array(
       'advert' => $advert,
-      'form'   => $form->createView(),
+      'form' => $form->createView() ,
     ));
-  }
+    }
 
-  public function deleteAction(Request $request, $id)
-  {
+
+  function deleteAction(Request $request, $id)
+    {
     $em = $this->getDoctrine()->getManager();
     $advert = $em->getRepository('ESIEAPlatformBundle:Advert')->find($id);
-    if (null === $advert) {
-      throw new NotFoundHttpException("La recette d'id ".$id." n'existe pas.");
-    }
-    
+    if (null === $advert)
+      {
+      throw new NotFoundHttpException("La recette d'id " . $id . " n'existe pas.");
+      }
+
     $form = $this->get('form.factory')->create();
-    if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
+    if ($request->isMethod('POST') && $form->handleRequest($request)->isValid())
+      {
       $em->remove($advert);
       $em->flush();
       $request->getSession()->getFlashBag()->add('info', "La recette a bien été supprimée.");
       return $this->redirectToRoute('esiea_platform_home');
-    }
-    
+      }
+
     return $this->render('ESIEAPlatformBundle:Advert:delete.html.twig', array(
       'advert' => $advert,
-      'form'   => $form->createView(),
+      'form' => $form->createView() ,
     ));
-  }
+    }
 
-  public function menuAction($limit)
-  {
+
+  function menuAction($limit)
+    {
     $em = $this->getDoctrine()->getManager();
-    $listAdverts = $em->getRepository('ESIEAPlatformBundle:Advert')->findBy(
-      array(),                 
-      array('date' => 'desc'), 
-      $limit,                  
-      0                        
-    );
+    $listAdverts = $em->getRepository('ESIEAPlatformBundle:Advert')->findBy(array() , array(
+      'date' => 'desc'
+    ) , $limit, 0);
     return $this->render('ESIEAPlatformBundle:Advert:menu.html.twig', array(
       'listAdverts' => $listAdverts
     ));
-  }
+    }
 
-  public function newdeliveryAction(Request $request)
+  
+  function newdeliveryAction(Request $request)
     {
-        $payment = new payment();
-        $form = $this->createForm(new paymentType(), $delivery);
-        $form->handleRequest($request);
+    $delivery = new delivery();
+    $form = $this->createForm(new paymentType() , $delivery);
+    $form->handleRequest($request);
+    if ($form->isValid())
+      {
+      $em = $this->getDoctrine()->getManager();
+      $em->persist($delivery);
+      $em->flush();
+      return $this->redirectToRoute('delivery');
+      }
+    }
 
-        if ($form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($delivery);
-            $em->flush();
-
-            return $this->redirectToRoute('payment');
-        }
-}
-}
+  function paymentupdateAction(Request $request)
+    {
+    if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()){
+     $request->getSession()->getFlashBag()->add('info', "La recette a bien été supprimée.");
+    
+    return $this->redirectToRoute('esiea_platform_home');
+    }
+    return $this->render('ESIEAPlatformBundle:Advert:index.html.twig');  
+    }
+  }
